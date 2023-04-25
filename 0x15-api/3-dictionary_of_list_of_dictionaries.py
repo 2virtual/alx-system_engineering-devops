e5bca747fcde4852d748e12376ccea2b9b5edf53
Olami <8sinss@gmail.com>
2023-04-24 21:51:57 -0700

    task03 updated

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -4,52 +4,70 @@ import json
 import requests
 import sys
 
+# Check if the employee ID was provided as a parameter
+if len(sys.argv) != 2:
+    print("Usage: {} EMPLOYEE_ID".format(sys.argv[0]))
+    sys.exit(1)
 
-def get_username(base_url, user_id):
-    """Gets username
-       Args:
-           base_url (str): base url for API
-           user_id (str): user id number
-       Returns: username
-    """
-    response = requests.get(
-        "{}users/{}".format(base_url, user_id), verify=False)
-    usr_dict = response.json()
-    return usr_dict['username']
+# Base URL of the API
+base_url = 'https://jsonplaceholder.typicode.com/'
 
+# Get the employee ID from the command line arguments
+employee_id = sys.argv[1]
 
-def get_complete_todo_list(base_url):
-    """Gets complete todo list
-       Args:
-           base_url (str): base url for API
-       Returns: list of todo items (dicts)
-    """
-    response = requests.get(
-        "{}todos".format(base_url), verify=False)
-    return response.json()
+# Make a request to the API to get the user information
+user_url = base_url + 'users/{}'.format(employee_id)
+user_response = requests.get(user_url)
 
+# Check if the user exists
+if user_response.status_code != 200:
+    print("Error: Employee with ID {} not found.".format(employee_id))
+    sys.exit(1)
 
-if __name__ == '__main__':
-    base_url = 'https://jsonplaceholder.typicode.com/'
-    users_dict = {}
-    todo_dict = {}
-    todo_list = get_complete_todo_list(base_url)
+# Parse the user information
+user_data = user_response.json()
+employee_name = user_data['name']
 
-    with open('todo_all_employees.json', 'w') as f:
-        uname = None
-        for task in todo_list:
-            user_id = str(task['userId'])
-            if user_id in users_dict.keys():
-                uname = users_dict[user_id]
-            else:
-                uname = get_username(base_url, user_id)
-                users_dict[user_id] = uname
-                todo_dict[user_id] = []
+# Make a request to the API to get the todo list information for the employee
+todo_url = base_url + 'todos?userId={}'.format(employee_id)
+todo_response = requests.get(todo_url)
 
-            todo = {}
-            todo['task'] = task['title']
-            todo['completed'] = task['completed']
-            todo['username'] = uname
-            todo_dict[user_id].append(todo)
+# Parse the todo list information
+todo_data = todo_response.json()
+tasks = []
 
-        f.write(json.dumps(todo_dict))
+# Find the number of completed tasks and the titles of those tasks
+for task in todo_data:
+    tasks.append({
+        "username": employee_name,
+        "task": task['title'],
+        "completed": task['completed']
+    })
+
+# Print the employee's progress report
+print("Employee {} is done with tasks({}/{}):".format(employee_name, len([t for t in tasks if t['completed']]), len(tasks)))
+
+# Export data in JSON format for all employees
+all_tasks = {}
+for user_id in range(1, 11):
+    user_url = base_url + 'users/{}'.format(user_id)
+    user_response = requests.get(user_url)
+    if user_response.status_code == 200:
+        user_data = user_response.json()
+        employee_name = user_data['name']
+        todo_url = base_url + 'todos?userId={}'.format(user_id)
+        todo_response = requests.get(todo_url)
+        todo_data = todo_response.json()
+        tasks = []
+        for task in todo_data:
+            tasks.append({
+                "username": employee_name,
+                "task": task['title'],
+                "completed": task['completed']
+            })
+        all_tasks[str(user_id)] = tasks
+
+# Write the data to a JSON file
+with open('todo_all_employees.json', 'w') as f:
+    json.dump(all_tasks, f)
+
