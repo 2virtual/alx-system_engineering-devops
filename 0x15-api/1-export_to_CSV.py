5c38664a75d763f355638d728d094a838f035a4b
Olami <8sinss@gmail.com>
2023-04-26 20:28:40 -0700

    update task1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-# script to gather todo data from an API and write to CSV file
 import csv
 import requests
 import sys
 
 # Check if the employee ID was provided as a parameter
 if len(sys.argv) != 2:
-    print("Usage: {} EMPLOYEE_ID".format(sys.argv[0]))  # Usage message
+    print("Usage: {} EMPLOYEE_ID".format(sys.argv[0]))
     sys.exit(1)
 
 # Base URL of the API
@@ -21,12 +20,13 @@ user_response = requests.get(user_url)
 
 # Check if the user exists
 if user_response.status_code != 200:
-    print("Error: Employee with ID {} not found.".format(employee_id))  # Error message
+    print("Error: Employee with ID {} not found.".format(employee_id))
     sys.exit(1)
 
-# Parse the user's information
+# Parse the user information
 user_data = user_response.json()
-employee_name = user_data['name']  # Employee name
+employee_name = user_data['name']
+employee_username = user_data['username']
 
 # Make a request to the API to get the todo list information for the employee
 todo_url = base_url + 'todos?userId={}'.format(employee_id)
@@ -34,26 +34,23 @@ todo_response = requests.get(todo_url)
 
 # Parse the todo list information
 todo_data = todo_response.json()
-number_of_done_tasks = 0
-total_number_of_tasks = len(todo_data)
-done_tasks = []
+tasks = []
 
 # Find the number of completed tasks and the titles of those tasks
 for task in todo_data:
-    if task['completed']:
-        number_of_done_tasks += 1
-        done_tasks.append(task['title'])
+    task_completed_status = "COMPLETED" if task['completed'] else "INCOMPLETE"
+    task_title = task['title']
+    tasks.append([employee_id, employee_username, task_completed_status, task_title])
+
+# Write the employee's tasks to a CSV file
+with open('{}.csv'.format(employee_id), mode='w') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow(['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
+    writer.writerows(tasks)
 
 # Print the employee's progress report
-print("Employee {} is done with tasks({}/{}):".format(employee_name, number_of_done_tasks, total_number_of_tasks))
-for task in done_tasks:
-    print("\t{}".format(task))
-
-# Export data to CSV file
-filename = "{}.csv".format(employee_id)  # CSV file name
-with open(filename, 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])  # CSV header
-    for task in todo_data:
-        writer.writerow([employee_id, employee_name, task['completed'], task['title']])  # CSV row
+print("Employee {} is done with tasks({}/{}):".format(employee_name, len([t for t in tasks if t[2] == "COMPLETED"]), len(tasks)))
+for task in tasks:
+    if task[2] == "COMPLETED":
+        print("\t{}".format(task[3]))
 
